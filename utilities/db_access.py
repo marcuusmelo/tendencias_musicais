a579c451a50d690d4bd556c18d8bc0ebeadbaa69
marcuusmelo <melo.mvinicius@gmail.com>
2020-08-05 21:51:09 -0300

    add db host and database to credentials file

--- utilities/db_access.py
+++ utilities/db_access.py
@@ -2,7 +2,7 @@ import sqlite3
 import psycopg2
 from sqlalchemy import create_engine
 
-from credentials import DB_USER, DB_PASSWORD
+from credentials import DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST
 
 def get_db_connection():
     conn = sqlite3.connect('/Users/marcusmelo/Desktop/projeto_m4_gh/tendencias_musicais_web/db.sqlite3')
@@ -13,7 +13,7 @@ def get_db_connection():
 
 def get_postgres_db_conn():
     conn = psycopg2.connect(
-        database='d8qa6a84s43m9m',
+        database=DB_DATABASE,
         user=DB_USER,
         password=DB_PASSWORD,
         host='ec2-52-204-20-42.compute-1.amazonaws.com',
@@ -26,9 +26,9 @@ def get_postgress_engine():
     engine_str = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(
         DB_USER,
         DB_PASSWORD,
-        'ec2-52-204-20-42.compute-1.amazonaws.com',
+        DB_HOST,
         '5432',
-        'd8qa6a84s43m9m'
+        DB_DATABASE
     )
     engine = create_engine(engine_str)
 
